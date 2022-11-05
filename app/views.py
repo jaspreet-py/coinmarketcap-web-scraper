0d0ba25c272f152d8ae3281285748e048da4cbc7
Jaspreet Singh <jaspreet159939@gmail.com>
2022-11-06 03:48:07 +0530

    Add serializer, view and url mapping for 'Currency' model

--- app/views.py
+++ app/views.py
@@ -1,3 +1,11 @@
 from django.shortcuts import render
+from rest_framework import viewsets
+
+from .models import Currency
+from .serializers import CurrencySerializer
+
 
 # Create your views here.
+class IndexView(viewsets.ModelViewSet):
+    serializer_class = CurrencySerializer
+    queryset = Currency.objects.all()
