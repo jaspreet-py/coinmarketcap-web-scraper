67ed4595228e7ff787ce8a1d7cc7a19dfb308f76
Jaspreet Singh <jaspreet159939@gmail.com>
2022-11-06 03:44:51 +0530

    Registered the 'Currency' model with admin

--- app/admin.py
+++ app/admin.py
@@ -1,3 +1,8 @@
 from django.contrib import admin
+from .models import Currency
 
 # Register your models here.
+class AppAdmin(admin.ModelAdmin):
+    list_display = ["name", "symbol", "price", "change", "mcap", "volume", "c_supply"]
+
+admin.site.register(Currency, AppAdmin)
