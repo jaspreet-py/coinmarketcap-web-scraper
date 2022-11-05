f1ed2b405356664b025dc01c8687c2cdf4ebbec3
Jaspreet Singh <jaspreet159939@gmail.com>
2022-11-06 03:43:52 +0530

    Add 'Currency' model to store crypto currency data

--- app/models.py
+++ app/models.py
@@ -1,3 +1,17 @@
 from django.db import models
 
 # Create your models here.
+class Currency(models.Model):
+    class Meta:
+        ordering: list[str] = ["-mcap"]
+
+    def __str__(self) -> str:
+        return self.name
+
+    name = models.CharField(max_length=100)
+    symbol = models.CharField(max_length=10)
+    price = models.FloatField()
+    change = models.JSONField()
+    mcap = models.BigIntegerField()
+    volume = models.JSONField()
+    c_supply = models.BigIntegerField()
